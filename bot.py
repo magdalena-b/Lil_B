8f2defa16afeab86b13dcc63e0e78dcbeb958e5e
magdalena <magdexxa@gmail.com>
2021-01-20 23:25:21 +0100

    Format with yapf

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import discord
 from discord.ext import commands
-import os 
+import os
 import random
 import asyncio
 import json
 import requests
 import urllib.request
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix='.')
 activating_inside_joke = False
 inside_jokes = []
+
 
 def contains_words(words, text):
     for word in words:
@@ -22,12 +23,14 @@ def get_inside_jokes():
     f = open('inside_jokes.txt')
     jokes = []
     jokes = f.read().split('\n')
-    return(jokes)
+    return (jokes)
+
 
 def add_inside_joke(joke):
     f = open('inside_jokes.txt', 'a')
     f.write('\n' + joke)
     inside_jokes.append(joke)
+
 
 def get_yt_query(message):
     query = ''
@@ -52,12 +55,12 @@ def get_seconds(message):
             if (i > 0):
                 if (splitted[i - 1].isdigit()):
                     hours = int(splitted[i - 1])
-        
+
         if element == 'm' or element == 'min' or element == 'minutes' or element == 'minut' or element == 'minuty':
             if (i > 0):
                 if (splitted[i - 1].isdigit()):
                     minutes = int(splitted[i - 1])
-        
+
         if element == 's' or element == 'sec' or element == 'seconds' or element == 'sekund' or element == 'sekundy':
             if (i > 0):
                 if (splitted[i - 1].isdigit()):
@@ -67,6 +70,7 @@ def get_seconds(message):
     seconds += minutes * 60
     seconds += hours * 3600
     return seconds
+
 
 def get_random(message):
     splitted = message.content.split(' ')
@@ -80,28 +84,31 @@ def get_random(message):
                 y = int(element)
     max = -1
     min = -1
-    if x < y: 
+    if x < y:
         min = x
         max = y
-    else: 
+    else:
         min = y
         max = x
 
     return random.randint(min, max)
+
 
 def get_panda_fact():
     panda_url = 'https://some-random-api.ml/facts/panda'
     fact = json.loads(requests.get(panda_url).content)["fact"]
     return fact
 
+
 def get_artist():
     f = open('music.txt')
     artists = []
     artists = f.read().split('\n')
-    return(artists[random.randint(0, len(artists))])
+    return (artists[random.randint(0, len(artists))])
+
 
 def get_joke(message):
-    return(message.content.replace("inside joke ", ""))
+    return (message.content.replace("inside joke ", ""))
 
 
 async def timer(seconds, message):
@@ -117,6 +124,7 @@ async def timer(seconds, message):
 async def send_meme(ctx):
     f = random.choice(os.listdir("memes"))
     await ctx.send(file=discord.File("memes/" + f))
+
 
 async def split_the_bill(message):
     msg = []
@@ -145,7 +153,6 @@ async def split_the_bill(message):
         else:
             output += " should pay " + str(round(diff, 2))
 
-
     await message.channel.send(output)
 
 
@@ -161,14 +168,16 @@ async def on_member_join(member):
     await member.send('Nice to see you here')
 
 
-
-
 @client.event
 async def on_member_remove(member):
     server = member.server
-    channel = [channel for channel in client.get_all_channels() if channel.name == 'ogólny'][0]
+    channel = [
+        channel for channel in client.get_all_channels()
+        if channel.name == 'ogólny'
+    ][0]
     message = 'bye {}! hope they come back'.format(member.mention)
     await client.send_message(channel, message)
+
 
 @client.event
 async def on_message(message):
@@ -177,11 +186,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    hi_words = ['yo', 'cześć', 'hej', 'hey', 'hi', 'dzień dobry', 'dzien dobry']
+    hi_words = [
+        'yo', 'cześć', 'hej', 'hey', 'hi', 'dzień dobry', 'dzien dobry'
+    ]
     if contains_words(hi_words, MSG):
         await message.channel.send('Yo')
 
-    bye_words = ['bye', 'do widzenia', 'żegnam', 'goodbye', 'see you later', 'peace']
+    bye_words = [
+        'bye', 'do widzenia', 'żegnam', 'goodbye', 'see you later', 'peace'
+    ]
     if contains_words(bye_words, MSG):
         await message.channel.send("Byee")
 
@@ -196,14 +209,17 @@ async def on_message(message):
 
     random_words = ['random']
     if contains_words(random_words, MSG):
-        random_n =  get_random(message)
+        random_n = get_random(message)
         await message.channel.send('Your random number is ' + str(random_n))
 
     panda_words = ['panda', 'pandy']
     if contains_words(panda_words, MSG):
         await message.channel.send(get_panda_fact())
 
-    music_words = ['recommend some music', 'recommend music', 'poleć muzykę', 'polec muzyke']
+    music_words = [
+        'recommend some music', 'recommend music', 'poleć muzykę',
+        'polec muzyke'
+    ]
     if contains_words(music_words, MSG):
         artist = get_artist()
         await message.channel.send("Personally i love " + artist)
@@ -219,21 +235,25 @@ async def on_message(message):
         add_inside_joke(joke)
         activating_inside_joke = True
 
-    count_members_words = ['count members', 'policz uzytkownikow', 'policz użytkowników']
+    count_members_words = [
+        'count members', 'policz uzytkownikow', 'policz użytkowników'
+    ]
     if contains_words(count_members_words, MSG):
-        await message.channel.send("There's " + str(message.guild.member_count) + " people and bots on this channel")
+        await message.channel.send("There's " +
+                                   str(message.guild.member_count) +
+                                   " people and bots on this channel")
 
-    how_are_you_words = ['how are you', 'jak się masz', 'jak sie masz', 'co tam', 'how\'s it going']
+    how_are_you_words = [
+        'how are you', 'jak się masz', 'jak sie masz', 'co tam',
+        'how\'s it going'
+    ]
     if contains_words(how_are_you_words, MSG):
         await message.channel.send("Oh you know it's going")
-
 
     if contains_words(inside_jokes, MSG) and not activating_inside_joke:
         await message.channel.send("LOL that's so funny")
 
-
     await client.process_commands(message)
-
 
 
 @client.command()
@@ -243,17 +263,20 @@ async def ping(ctx):
 
 @client.command()
 async def meme(ctx):
-        await send_meme(ctx)
+    await send_meme(ctx)
+
 
 @client.command()
 async def mem(ctx):
-        await send_meme(ctx)
+    await send_meme(ctx)
+
 
 @client.command()
 async def lil_b(ctx):
     info_file = open('lil_b_info.txt')
     info = info_file.read()
     await ctx.send(info)
+
 
 @client.command()
 async def lil_b_get_specific(ctx):
@@ -262,10 +285,9 @@ async def lil_b_get_specific(ctx):
     await ctx.send(info)
 
 
-
 def get_token():
-   f = open("token.txt")
-   return f.read() 
+    f = open("token.txt")
+    return f.read()
 
 
 token = get_token()
